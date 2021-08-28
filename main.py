ce497cab27cf35fe2b84024cd9f9c8717aa578b9
AtolMaker <atolbirdmaker@gmail.com>
2021-08-28 14:56:56 -0700

    Resources

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+names = {"eric": {"age": 31, "gender": "male"},
+        "blane": {"age": 23, "gender": "female"}}
+
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+
+    def post(self):
+        return {"data": "Posted"}
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
